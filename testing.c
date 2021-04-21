c8b33b3d89f236772c0bf0298a394289ec54fde2
Colton Hagan <haganc3@linux-10.cs.wwu.edu>
2021-04-21 10:15:04 -0700

    working in theory, not close to finial version

--- testing.c
+++ testing.c
@@ -16,108 +16,12 @@
 #include <string.h>
 #define PROTOPORT 36711 /* default protocol port number */
 #define QLEN 6 /* size of request queue */
-char localhost[] = "140.160.30.109";
+int visits = 0;
+char localhost[] = "local host";
 int main(int argc, char * argv[]) {
-        static struct option args[] = {
-                {
-                        "head",
-                        no_argument,
-                        NULL,
-                        0
-                },
-                {
-                        "tail",
-                        no_argument,
-                        NULL,
-                        1
-                },
-                {
-                        "rraddr",
-                        required_argument,
-                        NULL,
-                        2
-                },
-                {
-                        "rrport",
-                        required_argument,
-                        NULL,
-                        3
-                },
-                {
-                        "llport",
-                        required_argument,
-                        NULL,
-                        4
-                },
-                {
-                        NULL,
-                        0,
-                        NULL,
-                        0
-                }
-        };
-        char type[10] = "";
-        char raddr[1000] = "";
-        int lport = 0;
-        int rport = 0;
-        int opt = 0;
-        while ((opt = getopt_long_only(argc, argv, "", args, NULL)) != -1) {
-                switch (opt) {
-                case 0:
-                        if (type[0] != '\0') {
-                                printf("Error: Both -head and -tail roles requested");
-                                exit(EXIT_FAILURE);
-                        }
-                        strcpy(type, "head");
-                        break;
-                case 1:
-                        if (type[0] != '\0') {
-                                printf("Error: Both -head and -tail roles requested");
-                                exit(EXIT_FAILURE);
-                        }
-                        strcpy(type, "tail");
-                        break;
-                case 2:
-                        strcpy(raddr, optarg);
-                        break;
-                case 3:
-                        rport = atoi(optarg);
-                        if (rport < 1 || rport > 65535) {
-                                printf("Error: rrport address out of range \"value\"\n");
-                                exit(EXIT_FAILURE);
-                        }
-                        break;
-                case 4:
-                        lport = atoi(optarg);
-                        if (lport < 1 || lport > 65535) {
-                                printf("Error: llport address out of range \"value\"\n");
-                                exit(EXIT_FAILURE);
-                        }
-                        break;
-                default:
-                        exit(1);
-                }
-        }
-        if (type[0] == '\0') {
-                strcpy(type, "middle");
-        }
-
-        if (raddr[0] == '\0' && (strcmp(type, "middle") == 0)) {
-                printf("Error: Middle role requested -rraddr required\n");
-                exit(EXIT_FAILURE);
-        }
-
-        if (raddr[0] == '\0' && (strcmp(type, "head") == 0)) {
-                printf("Error: Head role requested -rraddr required\n");
-                exit(EXIT_FAILURE);
-        }
-
-        printf("type = %s\n", type);
-        printf("radr = %s\n", raddr);
-        printf("rport = %d\n", rport);
-        printf("lport = %d\n", lport);
-
-        if ((strcmp(type, "head") == 0)) {
+        strcpy(localhost, argv[2]);
+        if ((strcmp(argv[1], "client") == 0)) { //client
+                printf("client:\n");
                 struct hostent * ptrh; /* pointer to a host table entry */
                 struct protoent * ptrp; /* pointer to a protocol table entry */
                 struct sockaddr_in sad; /* structure to hold an IP address */
@@ -132,17 +36,18 @@ int main(int argc, char * argv[]) {
                 #endif
                 memset((char * ) & sad, 0, sizeof(sad)); /* clear sockaddr structure */
                 sad.sin_family = AF_INET; /* set family to Internet */
-                /* Check host argument and assign host name. */
-		port = PROTOPORT;
-		sad.sin_port = htons((u_short)port);
+                port = PROTOPORT;
+                sad.sin_port = htons((u_short)port);
                 host = localhost;
+                printf("port: %d\n", port);
+                printf("host: %s\n", host);
                 /* Convert host name to equivalent IP address and copy to sad. */
                 ptrh = gethostbyname(host);
                 if (((char * ) ptrh) == NULL) {
                         fprintf(stderr, "invalid host: %s\n", host);
                         exit(EXIT_FAILURE);
                 }
-                memcpy(&sad.sin_addr, ptrh -> h_addr, ptrh -> h_length);
+                memcpy( & sad.sin_addr, ptrh -> h_addr, ptrh -> h_length);
                 /* Map TCP transport protocol name to protocol number. */
                 if (((long int)(ptrp = getprotobyname("tcp"))) == 0) {
                         fprintf(stderr, "cannot map \"tcp\" to protocol number");
@@ -155,28 +60,27 @@ int main(int argc, char * argv[]) {
                         exit(EXIT_FAILURE);
                 }
                 /* Connect the socket to the specified server. */
-                if (connect(sd, (struct sockaddr*) &sad, sizeof(sad)) < 0) {
+                if (connect(sd, (struct sockaddr * ) & sad, sizeof(sad)) < 0) {
                         fprintf(stderr, "connect failed\n");
                         exit(EXIT_FAILURE);
                 }
-                //sprintf(buf,"Say hi!\n");
-                //send(sd,buf,strlen(buf),0);
-		char buffer[1000];
-    		while((n = read(STDIN_FILENO, buffer, sizeof(buffer))) > 0) {
-        		write(1,buffer,n);
-			send(sd,buffer,strlen(buffer),0);
-    		}
+                /* Repeatedly read data from socket and write to user's screen. */
+                n = recv(sd, buf, sizeof(buf), 0);
+                while (n > 0) {
+                        write(1, buf, n);
+                        n = recv(sd, buf, sizeof(buf), 0);
+                }
                 /* Close the socket. */
                 closesocket(sd);
                 /* Terminate the client program gracefully. */
                 exit(0);
-        } else if ((strcmp(type, "tail") == 0)) {
+        } else if ((strcmp(argv[1], "server") == 0)) { //server
+                printf("server:\n");
                 struct hostent * ptrh; /* pointer to a host table entry */
                 struct protoent * ptrp; /* pointer to a protocol table entry */
                 struct sockaddr_in sad; /* structure to hold server's address */
                 struct sockaddr_in cad; /* structure to hold client's address */
                 int sd, sd2; /* socket descriptors */
-                int n;
                 int port; /* protocol port number */
                 int alen; /* length of address */
                 char buf[1000]; /* buffer for string the server sends */
@@ -187,9 +91,9 @@ int main(int argc, char * argv[]) {
                 memset((char * ) & sad, 0, sizeof(sad)); /* clear sockaddr structure */
                 sad.sin_family = AF_INET; /* set family to Internet */
                 sad.sin_addr.s_addr = INADDR_ANY; /* set the local IP address */
-                port = PROTOPORT;
-		sad.sin_port = htons((u_short)port);
-		/* Map TCP transport protocol name to protocol number */
+                port = PROTOPORT; /* use default port number */
+                sad.sin_port = htons((u_short) port);
+                /* Map TCP transport protocol name to protocol number */
                 if (((long int)(ptrp = getprotobyname("tcp"))) == 0) {
                         fprintf(stderr, "cannot map \"tcp\" to protocol number");
                         exit(EXIT_FAILURE);
@@ -200,12 +104,6 @@ int main(int argc, char * argv[]) {
                         fprintf(stderr, "socket creation failed\n");
                         exit(EXIT_FAILURE);
                 }
-                /*Eliminate "Address already in use" error message.*/
-                int flag = 1;
-                /*if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, & flag, sizeof(int)) == -1) {
-                        fprintf(stderr, "set sock opt error\n");
-                        exit(1);
-                }*/
                 /* Bind a local address to the socket */
                 if (bind(sd, (struct sockaddr * ) & sad, sizeof(sad)) < 0) {
                         fprintf(stderr, "bind failed\n");
@@ -223,10 +121,10 @@ int main(int argc, char * argv[]) {
                                 fprintf(stderr, "accept failed\n");
                                 exit(EXIT_FAILURE);
                         }
-                        /* Prints out data --untested */
-                        while ((n = recv(sd2, buf, sizeof(buf), 0)) > 0) {
-            			write(1, buf, n);
-			}
+                        visits++;
+                        sprintf(buf, "This server has been contacted %d time%s\n",
+                                visits, visits == 1 ? "." : "s.");
+                        send(sd2, buf, strlen(buf), 0);
                         closesocket(sd2);
                 }
         }
