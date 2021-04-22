205afe5026b4da132039e0cbd5d8137b55743631
Colton Hagan <haganc3@linux-02.cs.wwu.edu>
2021-04-21 21:50:26 -0700

    more pretty

--- p1.c
+++ p1.c
@@ -61,6 +61,45 @@ int clientSocket (int port, char* host) {
     return sd;
 }
 
+int serverSocket (int port) {
+                int sd;
+                struct hostent * ptrh; /* pointer to a host table entry */
+                struct protoent * ptrp; /* pointer to a protocol table entry */
+                struct sockaddr_in sad;
+                memset((char * ) & sad, 0, sizeof(sad)); /* clear sockaddr structure */
+                sad.sin_family = AF_INET; /* set family to Internet */
+                sad.sin_addr.s_addr = INADDR_ANY; /* set the local IP address */
+                sad.sin_port = htons((u_short) port);
+                /* Map TCP transport protocol name to protocol number */
+                if (((long int)(ptrp = getprotobyname("tcp"))) == 0) {
+                        fprintf(stderr, "cannot map \"tcp\" to protocol number");
+                        exit(EXIT_FAILURE);
+                }
+                /* Create a socket */
+                sd = socket(PF_INET, SOCK_STREAM, ptrp -> p_proto);
+                if (sd < 0) {
+                        fprintf(stderr, "socket creation failed\n");
+                        exit(EXIT_FAILURE);
+                }
+                /*Eliminate "Address already in use" error message.*/
+                int flag = 1;
+                if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, & flag, sizeof(int)) == -1) {
+                        fprintf(stderr, "set sock opt error\n");
+                        exit(1);
+                }
+                /* Bind a local address to the socket */
+                if (bind(sd, (struct sockaddr * ) & sad, sizeof(sad)) < 0) {
+                        fprintf(stderr, "bind failed\n");
+                        exit(EXIT_FAILURE);
+                }
+                /* Specify size of request queue */
+                if (listen(sd, QLEN) < 0) {
+                        fprintf(stderr, "listen failed\n");
+                        exit(EXIT_FAILURE);
+                }
+        return sd;
+}
+
 int main(int argc, char * argv[]) {
         static struct option args[] = {
                 {
@@ -175,42 +214,15 @@ int main(int argc, char * argv[]) {
         WSAStartup(0x0101, & wsaData);
         #endif
         if ((strcmp(type, "head") == 0)) {
-                memset((char * ) & sad, 0, sizeof(sad)); /* clear sockaddr structure */
-                sad.sin_family = AF_INET; /* set family to Internet */
-                port = rport;
-                sad.sin_port = htons((u_short) port);
-                host = raddr;
-                /* Convert host name to equivalent IP address and copy to sad. */
-                ptrh = gethostbyname(host);
-                if (((char * ) ptrh) == NULL) {
-                        fprintf(stderr, "invalid host: %s\n", host);
-                        exit(EXIT_FAILURE);
-                }
-                memcpy( & sad.sin_addr, ptrh -> h_addr, ptrh -> h_length);
-                /* Map TCP transport protocol name to protocol number. */
-                if (((long int)(ptrp = getprotobyname("tcp"))) == 0) {
-                        fprintf(stderr, "cannot map \"tcp\" to protocol number");
-                        exit(EXIT_FAILURE);
-                }
-                /* Create a socket. */
-                sd = socket(PF_INET, SOCK_STREAM, ptrp -> p_proto);
-                if (sd < 0) {
-                        fprintf(stderr, "socket creation failed\n");
-                        exit(EXIT_FAILURE);
-                }
-                /* Connect the socket to the specified server. */
-                if (connect(sd, (struct sockaddr * ) & sad, sizeof(sad)) < 0) {
-                        fprintf(stderr, "connect failed\n");
-                        exit(EXIT_FAILURE);
-                }
-                while ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
-                        write(1, buf, n);
-                        send(sd, buf, n, 0);
-                }
-                /* Close the socket. */
-                closesocket(sd);
-                /* Terminate the client program gracefully. */
-                exit(0);
+            sd = clientSocket (rport, raddr);
+            while ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
+                write(1, buf, n);
+                send(sd, buf, n, 0);
+            }
+            /* Close the socket. */
+            closesocket(sd);
+            /* Terminate the client program gracefully. */
+            exit(0);
         } else if ((strcmp(type, "tail") == 0)) {
                 memset((char * ) & sad, 0, sizeof(sad)); /* clear sockaddr structure */
                 sad.sin_family = AF_INET; /* set family to Internet */
@@ -257,38 +269,7 @@ int main(int argc, char * argv[]) {
                         closesocket(sd2);
                 }
         } else if (strcmp(type, "middle") == 0) {
-                memset((char * ) & sad, 0, sizeof(sad)); /* clear sockaddr structure */
-                sad.sin_family = AF_INET; /* set family to Internet */
-                sad.sin_addr.s_addr = INADDR_ANY; /* set the local IP address */
-                port = lport;
-                sad.sin_port = htons((u_short) port);
-                /* Map TCP transport protocol name to protocol number */
-                if (((long int)(ptrp = getprotobyname("tcp"))) == 0) {
-                        fprintf(stderr, "cannot map \"tcp\" to protocol number");
-                        exit(EXIT_FAILURE);
-                }
-                /* Create a socket */
-                sd = socket(PF_INET, SOCK_STREAM, ptrp -> p_proto);
-                if (sd < 0) {
-                        fprintf(stderr, "socket creation failed\n");
-                        exit(EXIT_FAILURE);
-                }
-                /*Eliminate "Address already in use" error message.*/
-                int flag = 1;
-                if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, & flag, sizeof(int)) == -1) {
-                        fprintf(stderr, "set sock opt error\n");
-                        exit(1);
-                }
-                /* Bind a local address to the socket */
-                if (bind(sd, (struct sockaddr * ) & sad, sizeof(sad)) < 0) {
-                        fprintf(stderr, "bind failed\n");
-                        exit(EXIT_FAILURE);
-                }
-                /* Specify size of request queue */
-                if (listen(sd, QLEN) < 0) {
-                        fprintf(stderr, "listen failed\n");
-                        exit(EXIT_FAILURE);
-                }
+                sd = serverSocket(lport);
                 sd3 = clientSocket (rport, raddr);
                 /* Main server loop - accept and handle requests */
                 while (1) {
