e1f41cb1d761fefb08ef7ef14e2a1dd359005c83
Colton Hagan <haganc3@linux-02.cs.wwu.edu>
2021-04-21 17:40:26 -0700

    REMADE IT ALL AND IT WORKS WTF

--- p1.c
+++ p1.c
@@ -1,19 +1,30 @@
 #ifndef unix
-#define WIN32
-#include <windows.h>
+#define WIN3
+2#include <windows.h>
+
 #include <winsock.h>
+
 #else
 #define closesocket close
 #include <sys/types.h>
+
 #include <sys/socket.h>
+
 #include <netinet/in.h>
+
 #include <netdb.h>
+
 #endif
 #include <unistd.h>
+
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <getopt.h>
+
 #include <string.h>
+
 #define PROTOPORT 36711 /* my default port number*/
 #define QLEN 6 /* size of request queue */
 int main(int argc, char * argv[]) {
@@ -110,9 +121,9 @@ int main(int argc, char * argv[]) {
                 printf("Error: Head role requested -rraddr required\n");
                 exit(EXIT_FAILURE);
         }
-        
-        if(raddr[0] == '\0') {
-            strcpy(raddr, "localhost");
+
+        if (raddr[0] == '\0') {
+                strcpy(raddr, "localhost");
         }
 
         if ((strcmp(type, "head") == 0)) {
@@ -122,7 +133,7 @@ int main(int argc, char * argv[]) {
                 struct sockaddr_in sad; /* structure to hold an IP address */
                 int sd; /* socket descriptor */
                 int port; /* protocol port number */
-                char* host; /* pointer to host name */
+                char * host; /* pointer to host name */
                 int n; /* number of characters read */
                 char buffer[1000]; /* buffer for data from the server */
                 #ifdef WIN32
@@ -131,8 +142,8 @@ int main(int argc, char * argv[]) {
                 #endif
                 memset((char * ) & sad, 0, sizeof(sad)); /* clear sockaddr structure */
                 sad.sin_family = AF_INET; /* set family to Internet */
-		        port = rport;
-		        sad.sin_port = htons((u_short)port);
+                port = rport;
+                sad.sin_port = htons((u_short) port);
                 host = raddr;
                 /* Convert host name to equivalent IP address and copy to sad. */
                 ptrh = gethostbyname(host);
@@ -140,7 +151,7 @@ int main(int argc, char * argv[]) {
                         fprintf(stderr, "invalid host: %s\n", host);
                         exit(EXIT_FAILURE);
                 }
-                memcpy(&sad.sin_addr, ptrh -> h_addr, ptrh -> h_length);
+                memcpy( & sad.sin_addr, ptrh -> h_addr, ptrh -> h_length);
                 /* Map TCP transport protocol name to protocol number. */
                 if (((long int)(ptrp = getprotobyname("tcp"))) == 0) {
                         fprintf(stderr, "cannot map \"tcp\" to protocol number");
@@ -153,14 +164,14 @@ int main(int argc, char * argv[]) {
                         exit(EXIT_FAILURE);
                 }
                 /* Connect the socket to the specified server. */
-                if (connect(sd, (struct sockaddr*) &sad, sizeof(sad)) < 0) {
+                if (connect(sd, (struct sockaddr * ) & sad, sizeof(sad)) < 0) {
                         fprintf(stderr, "connect failed\n");
                         exit(EXIT_FAILURE);
                 }
-    		    while((n = read(STDIN_FILENO, buffer, sizeof(buffer))) > 0) {
-        		    write(1,buffer,n);
-			        send(sd,buffer,strlen(buffer),0);
-    		    }
+                while ((n = read(STDIN_FILENO, buffer, sizeof(buffer))) > 0) {
+                        write(1, buffer, n);
+                        send(sd, buffer, strlen(buffer), 0);
+                }
                 /* Close the socket. */
                 closesocket(sd);
                 /* Terminate the client program gracefully. */
@@ -184,8 +195,8 @@ int main(int argc, char * argv[]) {
                 sad.sin_family = AF_INET; /* set family to Internet */
                 sad.sin_addr.s_addr = INADDR_ANY; /* set the local IP address */
                 port = lport;
-		        sad.sin_port = htons((u_short)port);
-		        /* Map TCP transport protocol name to protocol number */
+                sad.sin_port = htons((u_short) port);
+                /* Map TCP transport protocol name to protocol number */
                 if (((long int)(ptrp = getprotobyname("tcp"))) == 0) {
                         fprintf(stderr, "cannot map \"tcp\" to protocol number");
                         exit(EXIT_FAILURE);
@@ -220,19 +231,20 @@ int main(int argc, char * argv[]) {
                                 exit(EXIT_FAILURE);
                         }
                         while ((n = recv(sd2, buf, sizeof(buf), 0)) > 0) {
-            			        write(1, buf, n);
-			            }
+                                write(1, buf, n);
+                        }
                         closesocket(sd2);
                 }
-        } else if (strcmp(type, "middle") == 0)  {
+        } else if (strcmp(type, "middle") == 0) {
                 printf("\nMiddle:\n");
                 struct hostent * ptrh; /* pointer to a host table entry */
                 struct protoent * ptrp; /* pointer to a protocol table entry */
-                struct sockaddr_in sad; /* structure to hold server's address */
+                struct sockaddr_in sad, sad2; /* structure to hold server's address */
                 struct sockaddr_in cad; /* structure to hold client's address */
-                int sd, sd2; /* socket descriptors */
+                int sd, sd2, sd3; /* socket descriptors */
                 int n;
-                int port; /* protocol port number */
+                int port, port2; /* protocol port number */
+                char * host; /* pointer to host name */
                 int alen; /* length of address */
                 char buf[1000]; /* buffer for string the server sends */
                 #ifdef WIN32
@@ -243,8 +255,8 @@ int main(int argc, char * argv[]) {
                 sad.sin_family = AF_INET; /* set family to Internet */
                 sad.sin_addr.s_addr = INADDR_ANY; /* set the local IP address */
                 port = lport;
-		        sad.sin_port = htons((u_short)port);
-		        /* Map TCP transport protocol name to protocol number */
+                sad.sin_port = htons((u_short) port);
+                /* Map TCP transport protocol name to protocol number */
                 if (((long int)(ptrp = getprotobyname("tcp"))) == 0) {
                         fprintf(stderr, "cannot map \"tcp\" to protocol number");
                         exit(EXIT_FAILURE);
@@ -271,7 +283,38 @@ int main(int argc, char * argv[]) {
                         fprintf(stderr, "listen failed\n");
                         exit(EXIT_FAILURE);
                 }
-		printf("Main loop...");
+                /*-------------------------------------- Code for new Socket--------------------*/
+                memset((char * ) & sad2, 0, sizeof(sad2)); /* clear sockaddr structure */
+                sad2.sin_family = AF_INET; /* set family to Internet */
+                port2 = rport;
+                sad2.sin_port = htons((u_short) port2);
+                host = raddr;
+                /* Convert host name to equivalent IP address and copy to sad. */
+                ptrh = gethostbyname(host);
+                if (((char * ) ptrh) == NULL) {
+                        fprintf(stderr, "invalid host: %s\n", host);
+                        exit(EXIT_FAILURE);
+                }
+                memcpy( & sad2.sin_addr, ptrh -> h_addr, ptrh -> h_length);
+                /* Map TCP transport protocol name to protocol number. */
+                if (((long int)(ptrp = getprotobyname("tcp"))) == 0) {
+                        fprintf(stderr, "cannot map \"tcp\" to protocol number");
+                        exit(EXIT_FAILURE);
+                }
+                /* Create a socket. */
+                sd3 = socket(PF_INET, SOCK_STREAM, ptrp -> p_proto);
+                if (sd < 0) {
+                        fprintf(stderr, "socket creation failed\n");
+                        exit(EXIT_FAILURE);
+                }
+                /* Connect the socket to the specified server. */
+                if (connect(sd3, (struct sockaddr * ) & sad2, sizeof(sad2)) < 0) {
+                        fprintf(stderr, "connect failed\n");
+                        exit(EXIT_FAILURE);
+                }
+                
+                /*-------------------------------------- New socket codecode --------------------*/
+                printf("Main loop...");
                 /* Main server loop - accept and handle requests */
                 while (1) {
                         printf("\nStart loop\n");
@@ -281,13 +324,12 @@ int main(int argc, char * argv[]) {
                                 exit(EXIT_FAILURE);
                         }
                         while ((n = recv(sd2, buf, sizeof(buf), 0)) > 0) {
-            			    write(1, buf, n);
-            			    send(sd2, buf, strlen(buf), 0);
-			}
+                                write(1, buf, n);
+                                send(sd3, buf, strlen(buf), 0);
+                        }
                         closesocket(sd2);
                         printf("End loop");
                 }
                 printf("Exiting\n");
         }
 }
-
